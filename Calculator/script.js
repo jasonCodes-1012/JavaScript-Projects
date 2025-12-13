a29433d32f2031967992912996889c875b8b6c20
Min Thant <minthantxcx@gmail.com>
2025-12-13 12:19:55 +0630

    Three Projects Finished

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,17 +1,17 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-    display.value += input;
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }catch(error){
-        display.value = "ERROR";``
-    }
+    display.value += input
 }
 
 function clearDisplay(){
     display.value = ""
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }catch(error){
+        display.value = "ERROR"
+    }
 }
